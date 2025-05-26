953f8b55f12fcc5fe2a96752af70c40b4a6c8a41
N E X U S <167659471+hoursinblack@users.noreply.github.com>
2025-05-26 13:34:53 +0700

    Create quizlethack.sh

---
+++ quizlethack.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Instantly quit Chrome
+osascript -e 'quit app "Google Chrome"'
+
+# Set system volume to max
+osascript -e 'set volume output volume 100'
+
+# Wait for dramatic effect
+sleep 2
+
+# Speak the message
+say "king von is so back"
